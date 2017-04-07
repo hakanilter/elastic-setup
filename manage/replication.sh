67beaf4610468dad7faf47c034df1d474dd7c767
Hakan Ilter <hakanilter@gmail.com>
2017-04-07 18:41:55 +0300

    fixed replication.sh

--- manage/replication.sh
+++ manage/replication.sh
@@ -16,4 +16,4 @@ if [ -z "$REPLICAS" ]
 fi
 
 echo "Setting index $INDEX to $REPLICAS"
-curl -XPUT "$ELASTIC_HOST/$INDEX/_settings" -d '{"index" : {"number_of_replicas" : ${REPLICAS}}}'
+curl -XPUT "$ELASTIC_HOST/$INDEX/_settings" -d "{"index" : {\"number_of_replicas\" : ${REPLICAS}}}"
